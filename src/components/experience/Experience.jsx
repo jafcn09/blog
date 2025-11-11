51b8a5727aa0dd8b4b087e884456792454292bfb
jafcn09 <jafcnepamace24@gmail.com>
2025-11-11 13:06:13 -0500

    Rediseño About y Experience con i18n y estilos animalista

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,212 +1,56 @@
 import React from 'react'
 import './experience.css'
 import { BsFillPatchCheckFill } from 'react-icons/bs'
+import { useTranslation } from 'react-i18next'
 
 const Experience = () => {
+  const { t } = useTranslation()
+
+  const skillsData = [
+    {
+      category: 'frontend',
+      icon: '🎨',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'SASS', 'Tailwind', 'Angular', 'React', 'UI/UX']
+    },
+    {
+      category: 'backend',
+      icon: '⚙️',
+      skills: ['Java Web', 'Spring Boot', 'REST API', 'AWS', 'Google Cloud', 'Azure', 'PHP', '.NET Core', 'Node.js']
+    },
+    {
+      category: 'databases',
+      icon: '🗄️',
+      skills: ['MySQL', 'SQL Server', 'PostgreSQL', 'MongoDB', 'Firebase', 'SQLite']
+    },
+    {
+      category: 'tools',
+      icon: '🛠️',
+      skills: ['Git/GitHub', 'VS Code', 'IntelliJ IDEA', 'Postman', 'Docker', 'Jira']
+    }
+  ]
+
   return (
-    <section id='experience'>
-      <h2>Mis Habilidades</h2>
+    <section id='experience' className='experience'>
+      <h2>{t('experience.title')}</h2>
 
-      {/*======== FRONT END ========*/}
       <div className='container experience__container'>
-        <div className='experience__frontend'>
-          <h3>FrontEnd Development</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>HTML</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>CSS</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>JavScript</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Bootstrap</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>SASS</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Materialize</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Tailwinds</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Angular</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Tymealeaf</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>DISEÑO UI / UX</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
+        {skillsData.map((skillGroup) => (
+          <div key={skillGroup.category} className='experience__category'>
+            <div className='experience__category-header'>
+              <span className='experience__category-icon'>{skillGroup.icon}</span>
+              <h3>{t(`experience.${skillGroup.category}`)}</h3>
+            </div>
 
+            <div className='experience__skills'>
+              {skillGroup.skills.map((skill) => (
+                <article key={skill} className='experience__skill'>
+                  <BsFillPatchCheckFill className='experience__skill-icon' />
+                  <span>{skill}</span>
+                </article>
+              ))}
+            </div>
           </div>
-        </div>
-        {/*======== BACK END ========*/}
-        <div className='experience__backend'>
-          <h3>BackEnd Development</h3>
-          <div className='experience__content'>
-
-
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>JavaWeb</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>SpringBoot</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>ApiRest</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Servlet</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-
-
-
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>AWS</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Google Cloud</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Azure</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Php</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>.Net Core</h4>
-              <small className='text-light'>INTERMEDIO</small>
-            </article>
-
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>ANGULARJS</h4>
-              <small className='text-light'>BASICO</small>
-            </article>
-          </div>
-        </div>
-
-        {/*======== BASES DE DATOS ========*/}
-        <div className='experience__backend'>
-          <h3>DataBases Development</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>MySQL</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>SQLServer</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-
-
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Postgress</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Apache Cassandra</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>MongoDB</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>SQlite</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-          </div>
-        </div>
-
-        {/*======== HERRAMIENTAS DE DESARROLLO ========*/}
-        <div className='experience__backend'>
-          <h3>Herramientas de desarrollos</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Visual Studio Code</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Intellij Idea</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Eclipse</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>NetBeans</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>GitHub / Git</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-            <article className='experience__details'>
-              <BsFillPatchCheckFill className='experience__details-icon' />
-              <h4>Postman &  SOAP UI</h4>
-              <small className='text-light'>AVANZADO</small>
-            </article>
-          </div>
-        </div>
-
+        ))}
       </div>
     </section>
   )
